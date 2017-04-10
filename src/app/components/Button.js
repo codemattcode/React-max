806e30264c8c36aa806ed483a48d0299c8e71b74
codemattcode <codemattcode@gmail.com>
2017-04-10 11:54:28 +0100

    style button

--- src/app/components/Button.js
+++ src/app/components/Button.js
@@ -5,7 +5,9 @@ export class Button extends React.Component {
 render() {
   const btnStyle = {
     marginTop: '10px',
-    padding: '10px 20px'
+    padding: '10px 20px',
+    background: 'rgb(109, 203, 35)',
+    border: '1px solid white'
   };
 return(
 <div>

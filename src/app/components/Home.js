4dcbfd1f467ddc36b8c8857a846f4934c0cd48ee
codemattcode <codemattcode@gmail.com>
2017-04-10 15:13:25 +0100

    refactor stateless component

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,19 +1,21 @@
 import React from "react";
 
-export class Home extends React.Component {
-render() {
+// stateless component
+export const Home =(props) => {
+
     const h1 = {
       fontWeight: '100',
       fontSize: '2.4em'
     };
-return(
 
+return(
 
 <div>
 
       <h1 style={h1}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore autem, nihil ipsam eius distinctio eos, necessitatibus porro, consequuntur tenetur sequi officiis! Hic debitis numquam quibusdam sequi modi. Assumenda, veritatis, modi.</h1>
 
 </div>
+
 );
-}
-}
+
+};

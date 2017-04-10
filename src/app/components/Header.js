c290fc5e502a5e9fa00021687030c15b269d9d0c
codemattcode <codemattcode@gmail.com>
2017-04-10 10:29:47 +0100

    cl

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -2,21 +2,13 @@ import React from "react";
 
 export class Header extends React.Component {
 render() {
+
 return(
-<div className='navbar navbar-default'>
-  <div className='container'>
-    <div className='navbar-header'>
-      <ul className='nav navbar-nav'>
-<li><a href='#'>{this.props.children}</a></li>
+
+<ul>
+  <li><a href='#'>{this.props.children}</a></li>
 </ul>
-</div>
-</div>
-</div>
+
 );
 }
 }
-
-// Deprecated
-// Header.propTypes = {
-//   children: React.PropTypes.element.isRequired
-// };
